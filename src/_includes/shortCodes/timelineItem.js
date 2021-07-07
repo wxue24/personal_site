94b23f516b76e9a09ea8d1da2bb00b70c57cbd1e
William <xuewilliam5@gmail.com>
2021-07-07 10:22:22 -0700

    Change projects.json structure and style projects

--- src/_includes/shortCodes/timelineItem.js
+++ src/_includes/shortCodes/timelineItem.js
@@ -13,7 +13,7 @@ const timelineItem = (side, title, time, description, tags) => {
       <div class="order-1 w-5/12 px-1 py-4 text-left">
         <p class="mb-3 text-base text-marine">${time}</p>
         <h4 class="mb-3 font-bold text-lg md:text-2xl">${title}</h4>
-        <p class="text-sm md:text-base leading-snug">${description}</p>
+        <p class="text-sm md:text-base text-black leading-snug">${description}</p>
       </div>
     </div>`
     : `<div
@@ -31,7 +31,7 @@ const timelineItem = (side, title, time, description, tags) => {
       <div class="order-1 w-5/12 px-1 py-4 text-right">
         <p class="mb-3 text-base text-marine">${time}</p>
         <h4 class="mb-3 font-bold text-lg md:text-2xl">${title}</h4>
-        <p class="text-sm md:text-base leading-snug">${description}</p>
+        <p class="text-sm md:text-base text-black leading-snug">${description}</p>
       </div>
     </div>`;
 };
